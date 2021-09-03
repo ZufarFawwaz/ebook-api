eaa1fac4c5ca7132e0b3fb206131ff4887067f33
unknown <ZufarFawwaz>
2021-09-03 20:27:30 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me()
+    {
+        return 
+        [
+            "NIS" => 3103119055,
+            "Nama" => "Zufar Fawwaz",
+            "Gender" => "Laki-laki",
+            "Phone" => 62821352512874,
+            "Class" => "XII RPL6"
+        ];
+    }
+}
